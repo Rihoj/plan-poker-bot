0e011b3b7a364fd849ad20fe6e5b02de9aa423b3
James Ray <jray@five-startech.com>
2018-02-09 00:19:36 -0500

    Update voters

--- app/Http/Controllers/PlanPokerController.php
+++ app/Http/Controllers/PlanPokerController.php
@@ -62,9 +62,6 @@ class PlanPokerController
         $votersHeader = isset($event['message']['cards'][2]['header']) ? $event['message']['cards'][2]['header'] : null;
         if ($votersHeader !== null) {
             $voters = explode(", ", $votersHeader['subtitle']);
-            if (in_array($user, $voters)) {
-                exit;
-            }
         }
         $actionResponse = new \stdClass;
         $actionResponse->type = "UPDATE_MESSAGE";
@@ -80,8 +77,10 @@ class PlanPokerController
         $section->widgets[] = new Widget($buttons);
         $this->response->cards[] = new Header("Plan Poker", $parameter['id']);
         $this->response->cards[] = $card;
-        $users = ($votersHeader !== null) ? $votersHeader['subtitle'].", ".$user : $user;
-        $this->response->cards[] = new Header("Voters", $users);
+        if (!in_array($user, $voters)) {
+            $voters[] = $user;
+        }
+        $this->response->cards[] = new Header("Voters", implode(", ", $voters));
         
         return ["actionResponse"=>$actionResponse, "cards"=> $this->response->cards];
     }
