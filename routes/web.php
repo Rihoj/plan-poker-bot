c9127c2b608e807fff3bac1b454403230f6bdb6a
James Ray <jray@five-startech.com>
2018-02-08 22:55:19 -0500

    try again

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,106 @@ $router->get('/', function () use ($router) {
     return getenv("APP_ENV");
 });
 
-$router->post('/', 'AppController@app');
-$router->post('/test', 'PlanPokerController@test');
+$router->post('/', function (\Illuminate\Http\Request $request) use ($router) {
+    $event = $request->json()->all();
+    if ($event["type"] === "ADDED_TO_SPACE" && $event['space']['type'] == 'ROOM') {
+        return ["text" => "Thanks for adding me!"];
+    } elseif ($event["type"] === "MESSAGE") {
+//        $header = ["header"=>[
+//          "title" => "Plan Poker",
+//          "subtitle" => $event['message']['text']
+//        ]];
+//        $button1 = [
+//            "textButton"=>[
+//                "text"=>"1",
+//                "onClick"=>[
+//                    "action"=>[
+//                        "actionMethodName"=>"vote",
+//                        "parameters"=>[
+//                            ["key"=> "topic", "value"=>$event['message']['text']],
+//                            ["key"=> "value", "value"=>"1"]
+//                        ]
+//                    ]
+//                ]
+//            ]
+//        ];
+//        $button2 = [
+//            "textButton"=>[
+//                "text"=>"2",
+//                "onClick"=>[
+//                    "action"=>[
+//                        "actionMethodName"=>"vote",
+//                        "parameters"=>[
+//                            ["key"=> "topic", "value"=>$event['message']['text']],
+//                            ["key"=> "value", "value"=>"2"]
+//                        ]
+//                    ]
+//                ]
+//            ]
+//        ];
+//        $button3 = [
+//            "textButton"=>[
+//                "text"=>"3",
+//                "onClick"=>[
+//                    "action"=>[
+//                        "actionMethodName"=>"vote",
+//                        "parameters"=>[
+//                            ["key"=> "topic", "value"=>$event['message']['text']],
+//                            ["key"=> "value", "value"=>"3"]
+//                        ]
+//                    ]
+//                ]
+//            ]
+//        ];
+//        $button4 = [
+//            "textButton"=>[
+//                "text"=>"5",
+//                "onClick"=>[
+//                    "action"=>[
+//                        "actionMethodName"=>"vote",
+//                        "parameters"=>[
+//                            ["key"=> "topic", "value"=>$event['message']['text']],
+//                            ["key"=> "value", "value"=>"5"]
+//                        ]
+//                    ]
+//                ]
+//            ]
+//        ];
+//        $button5 = [
+//            "textButton"=>[
+//                "text"=>"8",
+//                "onClick"=>[
+//                    "action"=>[
+//                        "actionMethodName"=>"vote",
+//                        "parameters"=>[
+//                            ["key"=> "topic", "value"=>$event['message']['text']],
+//                            ["key"=> "value", "value"=>"8"]
+//                        ]
+//                    ]
+//                ]
+//            ]
+//        ];
+//        $button6 = [
+//            "textButton"=>[
+//                "text"=>"13",
+//                "onClick"=>[
+//                    "action"=>[
+//                        "actionMethodName"=>"vote",
+//                        "parameters"=>[
+//                            ["key"=> "topic", "value"=>$event['message']['text']],
+//                            ["key"=> "value", "value"=>"13"]
+//                        ]
+//                    ]
+//                ]
+//            ]
+//        ];
+//        $buttons = ["buttons"=>[$button1, $button2, $button3, $button4, $button5, $button6]];
+//        $widgets = ["widgets"=>[$buttons]];
+//        $section = ["sections"=>[$widgets]];
+//        $cards = ["cards"=>[$header, $section]];
+        $planPoker = new App\Http\Controllers\PlanPokerController();
+        return $planPoker->start($event);
+    } elseif ($event["type"] == "CARD_CLICKED") {
+        return ["text"=>"Clicked"];
+    }
+});
